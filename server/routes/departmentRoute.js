2f07cca6093c45220a34b569a08de6ad0c152c00
Sven Dudink <sven@dudink.net>
2023-01-09 19:34:29 +0100

    added mongoDB connection in department registry  controller in backend

--- server/routes/departmentRoute.js
+++ server/routes/departmentRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { departments } from "../controller/departmentRegistry.js";
+import { departmentsSave } from "../controller/departmentRegistrySave.js";
 
 const router = express.Router();
 
 router.post("/", departments);
-router.post("/save", departments);
+router.post("/save", departmentsSave);
 //router.post("/save", employeeRegistrySave);
 
 export default router;
